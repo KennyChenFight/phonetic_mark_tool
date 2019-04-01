8f5b2c4599add27638aebc324a8e470c440d3453
KennyChen <kennychen851228@gmail.com>
2019-04-01 18:06:07 +0800

    加了一堆東西 怒

--- demo.py
+++ demo.py
@@ -1,92 +1,91 @@
 from phonetic import PhoneticMarkTool
-import os
-import sys
-from phonetic_mark_tool import Ui_MainWindow
-from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QPushButton, QMainWindow
-
-
-class AppWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.pb_recorded_file_dir.clicked.connect(self.recorded_file_dir_click)
-        self.ui.pb_recorded_mono_file_dir.clicked.connect(self.recorded_mono_file_dir_click)
-        self.ui.pb_mark_mono_file.clicked.connect(self.mark_mono_file_click)
-        self.ui.pb_record_file_dir.clicked.connect(self.record_file_dir_click)
-        self.ui.pb_record_full_dir.clicked.connect(self.record_full_file_dir_click)
-        self.ui.pb_mark_full_file.clicked.connect(self.mark_full_file_click)
-
-    def recorded_file_dir_click(self):
-        dir = QFileDialog.getExistingDirectory(self,
-                                               '選取資料夾',
-                                               './')
-        self.ui.le_recorded_file_dir.setText(dir)
-
-    def recorded_mono_file_dir_click(self):
-        dir = QFileDialog.getExistingDirectory(self,
-                                               '選取資料夾',
-                                               './')
-        self.ui.le_recorded_mono_dir.setText(dir)
-
-    def mark_mono_file_click(self):
-        filepath, filetype = QFileDialog. \
-            getSaveFileName(self,
-                            "選取文件",
-                            "./",
-                            "Text Files (*.txt)")
-        recorded_file_dir = self.ui.le_recorded_file_dir.text()
-        recorded_mono_file_dir = self.ui.le_recorded_mono_dir.text()
-        PhoneticMarkTool.mark_mono_file(recorded_file_dir, recorded_mono_file_dir, filepath)
-
-        QMessageBox.information(self,
-                                '>_<',
-                                '檔案產生成功!',
-                                QMessageBox.Yes)
-
-    def record_file_dir_click(self):
-        dir = QFileDialog.getExistingDirectory(self,
-                                               '選取資料夾',
-                                               './')
-        self.ui.le_record_file_dir.setText(dir)
-
-    def record_full_file_dir_click(self):
-        dir = QFileDialog.getExistingDirectory(self,
-                                               '選取資料夾',
-                                               './')
-        self.ui.le_record_full_dir.setText(dir)
-
-    def mark_full_file_click(self):
-        filepath, filetype = QFileDialog. \
-            getSaveFileName(self,
-                            "選取文件",
-                            "./",
-                            "Text Files (*.txt)")
-        record_file_dir = self.ui.le_record_file_dir.text()
-        record_full_file_dir = self.ui.le_record_full_dir.text()
-        PhoneticMarkTool.mark_full_file(record_file_dir, record_full_file_dir, filepath)
-
-        QMessageBox.information(self,
-                                '>_<',
-                                '檔案產生成功!',
-                                QMessageBox.Yes)
-
-
-app = QApplication(sys.argv)
-w = AppWindow()
-w.show()
-sys.exit(app.exec_())
-
-# recorded_file_dir = 'C:\\Users\\P19054\\Desktop\\recorded\\txt\\'
-# recorded_mono_dir = 'C:\\Users\\P19054\\Desktop\\recorded\\mono\\'
-# recorded_mark_file = 'C:\\Users\\P19054\\Desktop\\recorded\\mark.txt'
+# import os
+# import sys
+# from phonetic_mark_tool import Ui_MainWindow
+# from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QPushButton, QMainWindow
 #
-# record_file_dir = 'C:\\Users\\P19054\\Desktop\\407\\txt'
-# record_full_dir = 'C:\\Users\\P19054\\Desktop\\407\\full'
-# record_mark_file = 'C:\\Users\\P19054\\Desktop\\407\\mark.txt'
+#
+# class AppWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.ui = Ui_MainWindow()
+#         self.ui.setupUi(self)
+#         self.ui.pb_recorded_file_dir.clicked.connect(self.recorded_file_dir_click)
+#         self.ui.pb_recorded_mono_file_dir.clicked.connect(self.recorded_mono_file_dir_click)
+#         self.ui.pb_mark_mono_file.clicked.connect(self.mark_mono_file_click)
+#         self.ui.pb_record_file_dir.clicked.connect(self.record_file_dir_click)
+#         self.ui.pb_record_full_dir.clicked.connect(self.record_full_file_dir_click)
+#         self.ui.pb_mark_full_file.clicked.connect(self.mark_full_file_click)
+#
+#     def recorded_file_dir_click(self):
+#         dir = QFileDialog.getExistingDirectory(self,
+#                                                '選取資料夾',
+#                                                './')
+#         self.ui.le_recorded_file_dir.setText(dir)
+#
+#     def recorded_mono_file_dir_click(self):
+#         dir = QFileDialog.getExistingDirectory(self,
+#                                                '選取資料夾',
+#                                                './')
+#         self.ui.le_recorded_mono_dir.setText(dir)
+#
+#     def mark_mono_file_click(self):
+#         filepath, filetype = QFileDialog. \
+#             getSaveFileName(self,
+#                             "選取文件",
+#                             "./",
+#                             "Text Files (*.txt)")
+#         recorded_file_dir = self.ui.le_recorded_file_dir.text()
+#         recorded_mono_file_dir = self.ui.le_recorded_mono_dir.text()
+#         PhoneticMarkTool.mark_mono_file(recorded_file_dir, recorded_mono_file_dir, filepath)
+#
+#         QMessageBox.information(self,
+#                                 '>_<',
+#                                 '檔案產生成功!',
+#                                 QMessageBox.Yes)
+#
+#     def record_file_dir_click(self):
+#         dir = QFileDialog.getExistingDirectory(self,
+#                                                '選取資料夾',
+#                                                './')
+#         self.ui.le_record_file_dir.setText(dir)
+#
+#     def record_full_file_dir_click(self):
+#         dir = QFileDialog.getExistingDirectory(self,
+#                                                '選取資料夾',
+#                                                './')
+#         self.ui.le_record_full_dir.setText(dir)
+#
+#     def mark_full_file_click(self):
+#         filepath, filetype = QFileDialog. \
+#             getSaveFileName(self,
+#                             "選取文件",
+#                             "./",
+#                             "Text Files (*.txt)")
+#         record_file_dir = self.ui.le_record_file_dir.text()
+#         record_full_file_dir = self.ui.le_record_full_dir.text()
+#         PhoneticMarkTool.mark_full_file(record_file_dir, record_full_file_dir, filepath)
+#
+#         QMessageBox.information(self,
+#                                 '>_<',
+#                                 '檔案產生成功!',
+#                                 QMessageBox.Yes)
+#
+#
+# app = QApplication(sys.argv)
+# w = AppWindow()
+# w.show()
+# sys.exit(app.exec_())
 
+recorded_file_dir = 'C:\\Users\\P19054\\Desktop\\recorded\\txt\\'
+recorded_mono_dir = 'C:\\Users\\P19054\\Desktop\\recorded\\mono\\'
+recorded_mark_file = 'C:\\Users\\P19054\\Desktop\\recorded\\mark.txt'
 
-#PhoneticMarkTool.mark_mono_file(recorded_file_dir, recorded_mono_dir, recorded_mark_file)
+record_file_dir = 'C:\\Users\\P19054\\Desktop\\407\\txt'
+record_full_dir = 'C:\\Users\\P19054\\Desktop\\407\\full'
+record_mark_file = 'C:\\Users\\P19054\\Desktop\\407\\mark.txt'
+
+PhoneticMarkTool.mark_mono_file(recorded_file_dir, recorded_mono_dir, recorded_mark_file)
 #PhoneticMarkTool.mark_full_file(record_file_dir, record_full_dir, record_mark_file)
 
 # dir = 'C:\\Users\\P19054\\Desktop\\407\\full\\'
--- phonetic.py
+++ phonetic.py
@@ -1,4 +1,6 @@
 import os
+import statistics
+from decimal import Decimal
 
 
 class PhoneticMarkTool:
@@ -18,11 +20,12 @@ class PhoneticMarkTool:
         recorded_file_paths = cls.filepath_list(recorded_file_dir)
         mono_file_paths = cls.filepath_list(mono_file_dir)
         #recorded_file_paths, mono_file_paths = cls.check_same_sentence_file(recorded_file_paths, mono_file_paths)
-        recorded_table = cls.produce_recorded_table(recorded_file_paths, mono_file_paths)
+        recorded_table, wrong_filepaths = cls.produce_recorded_table(recorded_file_paths, mono_file_paths)
         c_table, v_table = cls.read_c_v_table()
         tone_table = cls.read_tone_table()
-        phonetic_table = cls.mark_phonetic(recorded_table, c_table, v_table, tone_table)
-        cls.write_mark_file(phonetic_table, mark_file)
+        phonetic_table, pron_dict = cls.mark_phonetic(recorded_table, c_table, v_table, tone_table)
+        cls.calculate_pron(mono_file_paths, wrong_filepaths, pron_dict)
+        # cls.write_mark_file(phonetic_table, mark_file)
 
     # 標記full file的注音
     @classmethod
@@ -62,46 +65,76 @@ class PhoneticMarkTool:
     def produce_recorded_table(cls,
                                recorded_file_paths,
                                mono_file_paths):
-        # text_set = {}
+        wrong_set = cls.check_wrong_mono_file(mono_file_paths)
         text_list = []
-        for filepath in recorded_file_paths:
-            with open(filepath, 'r', encoding='utf-8') as f:
-                line = ''.join([text for text in f.read() if not (text in cls.full_punctuation)])
-                text_list.append(line)
-                # if line in text_set:
-                #     print(filepath)
-                #     print(text_set[line])
-                #     print(line)
-                # else:
-                #     text_set[line] = filepath
+        for index, filepath in enumerate(recorded_file_paths):
+            if index not in wrong_set:
+                with open(filepath, 'r', encoding='utf-8') as f:
+                    line = ''.join([text for text in f.read() if not (text in cls.full_punctuation)])
+                    text_list.append(line)
+
         pron_list = []
+        for index, mono_filepath in enumerate(mono_file_paths):
+            if index not in wrong_set:
+                with open(mono_filepath, 'r', encoding='utf-8') as f:
+                    pron = []
+                    for line in f:
+                        line = line.strip()
+                        pron.append(''.join([pron for pron in line if not (pron.isdigit() or pron.isspace())]))
+                    pron_list.append(pron)
+
+        recorded_table = list(map(list, zip(text_list, pron_list)))
+        wrong_filepaths = [mono_file_paths[wrong]for wrong in wrong_set]
+        return recorded_table, wrong_filepaths
+
+    @classmethod
+    def check_wrong_mono_file(cls, mono_file_paths):
+        count = 0
+        time_collection = {}
         for mono_filepath in mono_file_paths:
             with open(mono_filepath, 'r', encoding='utf-8') as f:
-                pron = []
+                text_list = []
                 for line in f:
-                    line = line.strip()
-                    pron.append(''.join([pron for pron in line if not (pron.isdigit() or pron.isspace())]))
-                pron_list.append(pron)
-        #recorded_table = OrderedDict(zip(text_list, pron_list))
-        recorded_table = list(map(list, zip(text_list, pron_list)))
-        return recorded_table
+                    line = line.strip().split(' ')
+                    if 'pau' not in line:
+                        text_list.append([int(text) for text in line if text != '' and text.isdigit()])
+                time_collection[count] = text_list
+            count += 1
+
+        wrong_set = set()
+        for key, text_list in time_collection.items():
+            for time_list in text_list:
+                for time in time_list:
+                    if time < 0:
+                        wrong_set.add(key)
+                        break
+
+        for key, text_list in time_collection.items():
+            if key not in wrong_set:
+                for index, time_list in enumerate(text_list):
+                    if index + 1 < len(text_list):
+                        time1 = time_list[1]
+                        time2 = text_list[index+1][0]
+                        if time2 - time1 < -250000:
+                            print(time1)
+                            print(time2)
+                            print(os.path.basename(mono_file_paths[key]))
+                            wrong_set.add(key)
+                            break
+        print(len(wrong_set))
+        return wrong_set
+
 
     # 產生還沒錄音的句子與音節的對照表
     @classmethod
     def produce_record_table(cls,
                             record_file_paths,
                             full_file_paths):
-        # text_set = set()
         text_list = []
         for filepath in record_file_paths:
             with open(filepath, 'r', encoding='utf-8') as f:
                 line = f.read()
                 text_list.append(line)
-        #         if line in text_set:
-        #             print(line)
-        #         else:
-        #             text_set.add(line)
-        # print(len(text_set))
         pron_list = []
         for full_filepath in full_file_paths:
             with open(full_filepath, 'r', encoding='utf-8') as f:
@@ -111,7 +144,6 @@ class PhoneticMarkTool:
                     add_index = line.index('+')
                     pron.append(line[minus_index + 1:add_index])
                 pron_list.append(pron)
-        #recorded_table = OrderedDict(zip(text_list, pron_list))
         record_table = list(map(list, zip(text_list, pron_list)))
         return record_table
 
@@ -158,11 +190,13 @@ class PhoneticMarkTool:
     # 標記每個句子的注音，並產生對照表
     @classmethod
     def mark_phonetic(cls, record_table, c_table, v_table, tone_table):
+        pron_dict = {}
         for i, element in enumerate(record_table):
             sentence = element[0]
             pron_list = element[1]
             phonetic_collection = []
             index = 0
+            word_count = 0
             while index < len(pron_list):
                 pron = pron_list[index]
                 if pron in c_table:
@@ -174,6 +208,15 @@ class PhoneticMarkTool:
                             if value == tone:
                                 phonetic_list = [c_table[pron], v_table[pron_list[v1_index]], key]
                                 phonetic_collection.append(phonetic_list)
+
+                                word_pron = pron + ' ' + pron_list[v1_index] + ' ' + pron_list[v2_index]
+                                if word_pron in pron_dict:
+                                    pron_dict[word_pron][0] += 1
+                                    pron_dict[word_pron][1].add(sentence[word_count])
+                                    pron_dict[word_pron][2].append([i, index, index + 1, index + 2])
+                                else:
+                                    pron_dict[word_pron] = [1, set(sentence[word_count]), [[i, index, index + 1, index + 2]]]
+                                word_count += 1
                                 index += 3
                                 break
                     else:
@@ -188,6 +231,16 @@ class PhoneticMarkTool:
                             if value == tone:
                                 phonetic_list = [v_table[pron_list[v1_index]], key]
                                 phonetic_collection.append(phonetic_list)
+
+                                word_pron = pron_list[v1_index] + ' ' + pron_list[v2_index]
+                                if word_pron in pron_dict:
+                                    pron_dict[word_pron][0] += 1
+                                    pron_dict[word_pron][1].add(sentence[word_count])
+                                    pron_dict[word_pron][2].append([i, index, index + 1])
+                                else:
+                                    pron_dict[word_pron] = [1, set(sentence[word_count]), [[i, index, index + 1]]]
+
+                                word_count += 1
                                 index += 2
                                 break
                     else:
@@ -198,7 +251,8 @@ class PhoneticMarkTool:
                     index += 1
             print(sentence, phonetic_collection, pron_list)
             record_table[i][1] = phonetic_collection
-        return record_table
+        print(pron_dict)
+        return record_table, pron_dict
 
     # 將句子與注音的對照表進行寫檔
     @classmethod
@@ -210,3 +264,75 @@ class PhoneticMarkTool:
                 f.write(sentence + '\n')
                 f.write('   '.join([''.join(phonetic) for phonetic in phonetic_list]))
                 f.write('\n')
+
+    @classmethod
+    def calculate_pron(cls, mono_filepaths, wrong_filepath, pron_dict):
+        for pron, info in pron_dict.items():
+            count = info[0]
+            total_time = []
+            wrong_time_file_index = set()
+            for detail in info[2]:
+                filepath = mono_filepaths[detail[0]]
+                line_count = 0
+                time_start_index = detail[1]
+                time_end_index = detail[-1]
+                with open(filepath, 'r', encoding='utf-8') as f:
+                    time1 = 0
+                    time2 = 0
+                    for line in f:
+                        if line_count == time_start_index:
+                            time1 = int(line.strip().split(' ')[0])
+                        elif line_count == time_end_index:
+                            time2 = int(line.strip().split(' ')[-2])
+                            total_time.append((int(detail[0]) + 1, round((time2 - time1) / 10000000, 4)))
+                            break
+                        line_count += 1
+
+            if len(total_time) >= 2:
+                average_time = round(statistics.mean([time for index, time in total_time]), 4)
+                stdev_time = round(statistics.stdev([time for index, time in total_time]), 4)
+            elif len(total_time) == 1:
+                average_time = round(statistics.mean([time for index, time in total_time]), 4)
+                stdev_time = 0
+            else:
+                average_time = 0
+                stdev_time = 0
+
+            average_time = Decimal(str(average_time))
+            stdev_time = Decimal(str(stdev_time))
+            upbound_time = average_time + stdev_time
+            downbound_time = average_time - stdev_time
+
+            for index, time in total_time:
+                if time < downbound_time or time > upbound_time:
+                    wrong_time_file_index.add(index)
+
+            wrong_time_file_percentage = int(len(wrong_time_file_index) / count * 100)
+
+            pron_dict[pron].insert(1, average_time)
+            pron_dict[pron].insert(2, stdev_time)
+            pron_dict[pron].insert(3, upbound_time)
+            pron_dict[pron].insert(4, downbound_time)
+            pron_dict[pron].insert(5, wrong_time_file_percentage)
+            pron_dict[pron].insert(7, wrong_time_file_index)
+        print(pron_dict)
+
+        with open('calculate_pron.txt', 'w', encoding='utf-8') as f:
+            for pron, info in pron_dict.items():
+                f.write(pron + ',')
+                f.write(str(info[0]) + ',')
+                f.write(str(info[1]) + ',')
+                f.write(str(info[2]) + ',')
+                f.write(str(info[3]) + ',')
+                f.write(str(info[4]) + ',')
+                f.write(str(info[5]) + '%,')
+                f.write('、'.join(info[6]))
+                f.write(',')
+                if len(info[7]) != 0:
+                    f.write('、'.join([str(index) for index in info[7]]))
+                else:
+                    f.write('無問題句子')
+                f.write('\n')
+
+
+
